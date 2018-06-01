ad28b89f1e4e6684d93c22f25074a55ff3c62efc
Maissacrement <33574878+Maissacrement@users.noreply.github.com>
2018-06-01 19:43:01 +0100

    redefinition de l'objet + test de methodes

--- POO-php/index.php
+++ POO-php/index.php
@@ -1,8 +1,9 @@
 <?php
     require 'Personnage.php';
 
-    $perso = new Personnage("courtney");
+    $perso = new Personnage("courtney", 10);
     echo "Bonjour ".$perso->getName();
     var_dump($perso);
     var_dump($perso->estMajeur());
+    $perso->aimeLaMusic("Y");
 ?>
